e8a3fd9dc28877f19768eadb9e970c6587d4847e
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-07-13 12:28:37 -0300

    add

--- main.js
+++ main.js
@@ -1,94 +1,82 @@
-document.querySelector("#open-nav-menu").addEventListener("click", function () {
-  document.querySelector("header nav .wrapper").classList.add("nav-open");
-});
-
-document
-  .querySelector("#close-nav-menu")
-  .addEventListener("click", function () {
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+function menuHandler() {
+  document.querySelector("#open-nav-menu").addEventListener("click", function () {
+    document.querySelector("header nav .wrapper").classList.add("nav-open");
   });
 
-let linksA = document.querySelectorAll("a");
-console.log(linksA[4].hash);
+  document
+    .querySelector("#close-nav-menu")
+    .addEventListener("click", function () {
+      document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
 
-linksA.forEach(function (linksfunc) {
-  linksfunc.addEventListener("click", function () {
-    /*    alert(wrapper.hash); */
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+  var linksA = document.querySelectorAll("a");
+
+  linksA.forEach(function (linksfunc) {
+    linksfunc.addEventListener("click", function () {
+      /*    alert(wrapper.hash); */
+      document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
   });
-});
-
+}
 let greetingText = "";
+function dateAtt() {
+  let currentHour = new Date().getHours();
+  if (currentHour < 12) {
+    greetingText = "Bom dia!";
+  } else if (currentHour < 19) {
+    greetingText = "Boa Tarde!";
+  } else if (currentHour < 24) {
+    greetingText = "Boa Noite!";
+  } else {
+    greetingText = "Bem vindo!";
 
-let currentHour = new Date().getHours();
-console.log(currentHour);
-if (currentHour < 12){
-  greetingText = "Bom dia!";
-}else if (currentHour < 19){
-  greetingText = "Boa Tarde!";
-}else if (currentHour <24 ){
-  greetingText = "Boa Noite!";
-}else {
-  greetingText = "Bem vindo!";
+  }
+  //data
+  setInterval(function () {
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").innerHTML = localTime
+      .getHours()
+      .toString()
+      .padStart(2, "0");
+    document.querySelector("span[data-time=minutes]").innerHTML = localTime
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    document.querySelector("span[data-time=seconds]").innerHTML = localTime
+      .getSeconds()
+      .toString()
+      .padStart(2, "0");
+  }, 1000);
 
 }
+function temperatureDisplay() {
+  const weatherCondition = "chuvoso";
+  const userLocation = "São Paulo";
+  let temperature = 27.6;
+  let celsiusText = `O dia está ${weatherCondition} em ${userLocation} e com temperatura de ${temperature.toFixed(
+    1
+  )} °C.`;
+  let fahr = (temperature * 9) / 5 + 32;
 
+  let fahrText = `O dia está ${weatherCondition} em ${userLocation} e com temperatura de ${fahr.toFixed(
+    1
+  )} Fahr.`;
 
+  document.querySelector("#greeting").innerHTML = greetingText;
 
+  document.querySelector("#weather").innerHTML = celsiusText;
 
-const weatherCondition = "chuvoso";
-const userLocation = "São Paulo";
-let temperature = 27.6;
-let celsiusText = `O dia está ${weatherCondition} em ${userLocation} e com temperatura de ${temperature.toFixed(
-  1
-)} °C.`;
-let fahr = (temperature * 9) / 5 + 32;
-
-let fahrText = `O dia está ${weatherCondition} em ${userLocation} e com temperatura de ${fahr.toFixed(
-  1
-)} Fahr.`;
-
-document.querySelector("#greeting").innerHTML = greetingText;
-
-document.querySelector("#weather").innerHTML = celsiusText;
-
-document
-  .querySelector(".weather-group")
-  .addEventListener("click", function (e) {
-    if (e.target.id === "celsius") {
-      document.querySelector("#weather").innerHTML = celsiusText;
-    } else if (e.target.id === "fahr") {
-      document.querySelector("#weather").innerHTML = fahrText;
-    }
-  });
-
-console.log("fora timeout");
-setInterval(function () {
-  let localTime = new Date();
-  document.querySelector("span[data-time=hours]").innerHTML = localTime
-    .getHours()
-    .toString()
-    .padStart(2, "0");
-  document.querySelector("span[data-time=minutes]").innerHTML = localTime
-    .getMinutes()
-    .toString()
-    .padStart(2, "0");
-  document.querySelector("span[data-time=seconds]").innerHTML = localTime
-    .getSeconds()
-    .toString()
-    .padStart(2, "0");
-}, 1000);
-
-let animals = ["dog", "cat", "lion", "fish"];
-
-for (let a = 0; a < animals.length; a++) {
-  console.log(animals[a]);
+  document
+    .querySelector(".weather-group")
+    .addEventListener("click", function (e) {
+      if (e.target.id === "celsius") {
+        document.querySelector("#weather").innerHTML = celsiusText;
+      } else if (e.target.id === "fahr") {
+        document.querySelector("#weather").innerHTML = fahrText;
+      }
+    });
 }
-
-for (let a in animals) {
-  console.log("animals: " + animals[a]);
-}
-
+//gallery
 const galleryImages = [
   {
     src: "./assets/gallery/image3.jpg",
@@ -102,41 +90,155 @@ const galleryImages = [
     src: "./assets/gallery/image2.jpg",
     alt: "Thimbail Image 2",
   },
- 
+
 ];
+const products = [
+  {
+    title: 'AstroFiction',
+    author: "John Doe",
+    price: 49.9,
+    image: "./assets/products/img6.png",
+  },
+  {
+    title: 'Space Odissey',
+    author: "Marie Anne",
+    price: 35,
+    image: "./assets/products/img1.png",
+  },
+  {
+    title: 'Doomed City',
+    author: "Jason Cobert",
+    price: 0,
+    image: "./assets/products/img2.png",
+  },
+  {
+    title: 'Black Dog',
+    author: "John Doe",
+    price: 85.35,
+    image: "./assets/products/img3.png",
+  },
+  {
+    title: 'My Little Robot',
+    author: "Pedro Paulo",
+    price: 0,
+    image: "./assets/products/img5.png",
+  },
+  {
+    title: 'Garden Girl',
+    author: "Ankit Patel",
+    price: 45,
+    image: "./assets/products/img4.png",
+  },
+]
+function thumbnailDisplay() {
+  let mainImage = document.querySelector("#gallery > img");
+  let thumbnails = document.querySelector("#gallery > .thumbnails");
+  mainImage.src = galleryImages[0].src;
+  mainImage.alt = galleryImages[0].alt;
 
-/*    for(let i in galleryImages){
-    console.log(galleryImages[i].src);
-    console.log(galleryImages[i].alt);
-   } */
-/*    <img
-   src="./assets/gallery/image1.jpg"
-   alt="Thumbnail Image 1"
-   data-array-index="0"
-   data-selected="true"
- /> */
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery > .thumbnails");
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+  //selecionar gallery images
+  galleryImages.forEach(function (image, index) {
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.dataset.arrayIndex = index;
+    thumb.dataset.selected = index === 0 ? true : false;
+    thumb.addEventListener("click", function (e) {
+      let selectIndex = e.target.dataset.arrayIndex;
+      let selectedImage = galleryImages[selectIndex];
+      mainImage.src = selectedImage.src;
+      mainImage.alt = selectedImage.alt;
+      thumbnails.querySelectorAll("img").forEach(function (img) {
+        img.dataset.selected = false;
 
-galleryImages.forEach(function (image, index) {
-  let thumb = document.createElement("img");
-  thumb.src = image.src;
-  thumb.alt = image.alt;
-  thumb.dataset.arrayIndex = index;
-  thumb.dataset.selected = index === 0 ? true : false;
-  thumb.addEventListener("click", function (e) {
-    let selectIndex = e.target.dataset.arrayIndex;
-    let selectedImage = galleryImages[selectIndex];
-    mainImage.src = selectedImage.src;
-    mainImage.alt = selectedImage.alt;
-    thumbnails.querySelectorAll("img").forEach(function(img){
-      img.dataset.selected = false;
+      });
+      e.target.dataset.selected = true;
 
     });
-    e.target.dataset.selected = true;
+    thumbnails.appendChild(thumb);
+  });
+}
 
-    });
-  thumbnails.appendChild(thumb);
+
+function productsHandler() {
+  let productsSection = document.querySelector(".products-area");
+  products.forEach(function (product, index) {
+    let productElm = document.createElement("div");
+    productElm.classList.add("product-item");
+    let productImage = document.createElement("img");
+    productImage.src = product.image;
+    productImage.alt = "Image for product " + product.title;
+
+    let productDetails = document.createElement("div");
+    productDetails.classList.add("product-details");
+    
+    let productTitle = document.createElement("h3");
+    productTitle.classList.add("product-title");
+    productTitle.textContent = product.title;
+
+    let productAuthor = document.createElement("p");
+    productAuthor.classList.add("product-author");
+    productAuthor.textContent = product.author;
+  
+    let priceTitle = document.createElement("p");
+    priceTitle.classList.add("price-title");
+    priceTitle.textContent = "Preço";
+
+    let productPrice = document.createElement("p");
+    productPrice.classList.add("product-price");
+    productPrice.textContent = product.price > 0 ? "R$ "+product.price.toFixed(2) : "Grátis";
+
+
+    productDetails.appendChild(productTitle);
+    productDetails.appendChild(productAuthor);
+    productDetails.appendChild(priceTitle);
+    productDetails.appendChild(productPrice);
+
+    productElm.append(productImage);
+
+    productElm.append(productDetails);
+    
+    productsSection.append(productElm);
+  });
+
+
+let freeProducts = products.filter(function(item){
+  return !item.price || item.price <= 0;
 });
+
+let paidProducts = products.filter(function(item){
+  return item.price > 0;
+});
+console.log("free", freeProducts)
+console.log("paid", paidProducts)
+
+products.forEach(function(product, index){
+products
+});
+document.querySelector(".products-filter label[for=all] span.product-amount").textContent = products.length;
+document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = paidProducts.length;
+document.querySelector(".products-filter label[for=free] span.product-amount").textContent = freeProducts.length;
+
+
+
+};
+
+
+
+
+//page load
+menuHandler();
+dateAtt();
+productsHandler();
+temperatureDisplay();
+thumbnailDisplay();
+//products section
+/* <div class="product-item">
+<img src="./assets/products/img6.png" alt="AstroFiction" />
+<div class="product-details">
+  <h3 class="product-title">AstroFiction</h3>
+  <p class="product-author">John Doe</p>
+  <p class="price-title">PREÇO</p>
+  <p class="product-price">R$ 49.90</p>
+</div>
+</div> */
